211e65cb5001a6c0cef2395b6f6323d264928526
koudai0024 <iwai.0731v@gmail.com>
2021-06-01 22:54:43 +0900

    💄 トップページのグラフ部分のui作成と、リスト部分作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/solid";
 import { styled } from "linaria/react";
+import { VictoryPie } from "victory";
 
 const Container = styled.div`
   background: #eff6ff;
@@ -69,10 +71,107 @@ const MainCardMoney = styled.p`
   }
 `;
 
+const ChartWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+  width: 100%;
+  margin-top: 24px;
+`;
+
+const ChartDisplay = styled.div`
+  background: #fff;
+  border-radius: 18px;
+  flex: 1;
+  box-shadow: 4px 4px 15px 0px rgba(0, 0, 0, 0.2);
+`;
+
+const TransactionsWrap = styled.div`
+  width: 100%;
+  margin-top: 32px;
+`;
+
+const TransactionsTitle = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  color: #1f2937;
+`;
+
+const TransactionsList = styled.ul`
+  width: 100%;
+  margin-top: 12px;
+`;
+
+const TransactionsItem = styled.li`
+  display: flex;
+  margin-top: 12px;
+`;
+
+const TransactionsIcon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 48px;
+  border-radius: 8px;
+
+  &.income {
+    background: #5ca2d5;
+  }
+  &.spending {
+    background: #f68887;
+  }
+
+  .icon {
+    width: 32px;
+    height: 32px;
+    color: #fff;
+  }
+`;
+
+const TransactionsDesc = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 4px;
+  font-weight: bold;
+  margin-left: 12px;
+
+  .label {
+    font-size: 16px;
+    color: #111827;
+  }
+
+  .date {
+    font-size: 14px;
+    color: #6b7280;
+  }
+`;
+
+const TransactionsMoney = styled.p`
+  display: flex;
+  align-items: center;
+  font-size: 24px;
+  font-weight: bold;
+  color: #111827;
+  margin-left: auto;
+
+  .inner {
+    display: flex;
+    align-items: flex-end;
+    gap: 4px;
+  }
+  .yen {
+    font-size: 14px;
+  }
+`;
+
 const Home = () => {
   return (
     <Container>
       <MonthSelecter type="month" value="2021-05" />
+
       <MainCard>
         <MainCardInner>
           <MainCardItem>
@@ -93,6 +192,63 @@ const Home = () => {
           </MainCardItem>
         </MainCardInner>
       </MainCard>
+
+      <ChartWrap>
+        <ChartDisplay>
+          <VictoryPie />
+        </ChartDisplay>
+        <ChartDisplay>
+          <VictoryPie />
+        </ChartDisplay>
+      </ChartWrap>
+
+      <TransactionsWrap>
+        <TransactionsTitle>Transactions</TransactionsTitle>
+        <TransactionsList>
+          <TransactionsItem>
+            <TransactionsIcon className="spending">
+              <ArrowUpIcon className="icon" />
+            </TransactionsIcon>
+            <TransactionsDesc>
+              <p className="label">昼食すき家</p>
+              <p className="date">5/4</p>
+            </TransactionsDesc>
+            <TransactionsMoney>
+              <span className="inner">
+                -450<span className="yen">円</span>
+              </span>
+            </TransactionsMoney>
+          </TransactionsItem>
+          <TransactionsItem>
+            <TransactionsIcon className="income">
+              <ArrowDownIcon className="icon" />
+            </TransactionsIcon>
+            <TransactionsDesc>
+              <p className="label">昼食すき家</p>
+              <p className="date">5/4</p>
+            </TransactionsDesc>
+            <TransactionsMoney>
+              <span className="inner">
+                -450<span className="yen">円</span>
+              </span>
+            </TransactionsMoney>
+          </TransactionsItem>
+          <TransactionsItem>
+            <TransactionsIcon className="spending">
+              <ArrowUpIcon className="icon" />
+            </TransactionsIcon>
+            <TransactionsDesc>
+              <p className="label">昼食すき家</p>
+              <p className="date">5/4</p>
+            </TransactionsDesc>
+            <TransactionsMoney>
+              <span className="inner">
+                -450<span className="yen">円</span>
+              </span>
+            </TransactionsMoney>
+          </TransactionsItem>
+        </TransactionsList>
+      </TransactionsWrap>
     </Container>
   );
 };
