7e9c23bd87805441db0e7cf2e26910698e558b25
koudai0024 <iwai.0731v@gmail.com>
2021-06-01 17:07:47 +0900

    ✨ reset.cssの適応と、linariaのセットアップ

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withLinaria = require("next-linaria");
+
+module.exports = withLinaria({
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
   poweredByHeader: false,
-};
+});
